4cc7534d89158c76ef0fcca7e72066c5437a7dc4
yoosion030 <rsy5487@naver.com>
2022-07-06 16:58:25 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+import Text from 'components/Test'
+
+const IndexPage: FunctionComponent = function () {
+  return <Text text="Home" />
+}
+
+export default IndexPage
